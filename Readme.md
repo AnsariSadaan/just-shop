6d3ecf2ee8265bd194b1995cf669d346e3ef4814
sadaan <ansarisadaan72@gmail.com>
2024-06-12 15:44:26 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# just - shop e-commerce website
+
+this is a ecommerce website in mern stack 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hust shop</h1>
+    </>
+  )
+}
+
+export default App
