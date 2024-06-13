f1b050bde2c3412301fa95d84da81928bcbd70f0
sadaan <ansarisadaan72@gmail.com>
2024-06-13 13:12:32 +0530

    utils code and added some dependecies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'});
 import dotenv from 'dotenv';
 import connectDB from "./db/db_connect.js";
+import { app } from './app.js';
 
 
 dotenv.config({
@@ -8,4 +9,12 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 7000, ()=> {
+        console.log(`Server is Running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=> {
+    console.log("MONGODB Connection failed !!", err);
+})
