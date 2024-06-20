f2f111027f92ca2fda58f0120f334d003e53a4dd
sadaan <ansarisadaan72@gmail.com>
2024-06-20 20:07:32 +0530

    added outlet for rendering children from routes including header footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-red-500'>just shop</h1>
+      <Header />
+      <main className='min-h-[calc(100vh-120px)]'>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   )
 }
