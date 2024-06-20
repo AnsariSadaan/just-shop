662360c5035e3ddb38e9f5f09f34b7e5554b99cd
sadaan <ansarisadaan72@gmail.com>
2024-06-20 12:50:56 +0530

    user model created

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    avatar: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true })
+
+export const User = mongoose.model("User", userSchema)
