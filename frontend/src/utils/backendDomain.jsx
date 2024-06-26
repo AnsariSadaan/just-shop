34a4cb56ceca6dd23b7df410fa58fafe419c3644
sadaan <ansarisadaan72@gmail.com>
2024-06-26 12:54:31 +0530

    added backend api link

---
+++ frontend/src/utils/backendDomain.jsx
@@ -0,0 +1,10 @@
+const backendDomain = 'http://localhost:7000'
+
+const summaryApi = {
+    signUp :{
+        url: `${backendDomain}/api/signup`,
+        method: "post"
+    }
+}
+
+export default summaryApi;
