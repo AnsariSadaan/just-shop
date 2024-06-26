a2edf06ee6fd04db008e94b7c23288b5e8416422
sadaan <ansarisadaan72@gmail.com>
2024-06-26 12:41:25 +0530

    changed route position and icreased limit of data upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import router from './routes/index.js'
 const app = express()
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    Credential: true,
+    credential: true,
 }));
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "100mb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
+
+app.use('/api', router);
 
 export {app}
