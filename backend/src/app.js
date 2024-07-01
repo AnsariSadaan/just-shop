136376944b98369a7a16666e219387c875470c64
sadaan <ansarisadaan72@gmail.com>
2024-07-01 15:58:20 +0530

    signin routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,13 @@ import router from './routes/index.js'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credential: true,
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
 }));
 app.use(express.json({limit: "100mb"}))
+app.use(cookieParser());
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser());
 
 app.use('/api', router);
 
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import useSignUpController from '../controllers/userSignUp.js';
+import userSignInController from '../controllers/userSignIn.js';
 
 
 const router = express.Router();
-router.post('/signup', useSignUpController) 
+router.post('/signup', useSignUpController);
+router.post('/signin', userSignInController); 
 export default router;
