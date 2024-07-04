925725a35c5cabc744ae0f946037df34b8110e8a
sadaan <ansarisadaan72@gmail.com>
2024-07-04 12:54:11 +0530

    configured admin panel routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,8 @@ import userSignInController from '../controllers/userSignIn.js';
 import userDetailsController from '../controllers/userDetails.js';
 import authToken from '../middlewares/authToken.js';
 import userLogout from '../controllers/userLogout.js';
+import AllUsers from '../controllers/AllUsers.js';
+import UpdateUserRole from '../controllers/UpdateUserRole.js';
 
 
 const router = express.Router();
@@ -11,4 +13,8 @@ router.post('/signup', useSignUpController);
 router.post('/signin', userSignInController); 
 router.get('/user-details', authToken, userDetailsController);
 router.get('/userLogout', userLogout);
+
+// admin panel 
+router.get('/all-users',authToken, AllUsers);
+router.post('/update-user', authToken, UpdateUserRole)
 export default router;
