727201ba05eddc2e23e07d1e12d6e8ec58e9f612
sadaan <ansarisadaan72@gmail.com>
2024-07-05 20:26:20 +0530

    crated all product page for updating product

--- frontend/src/pages/AllProducts.jsx
+++ frontend/src/pages/AllProducts.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UploadProduct from '../components/UploadProduct'
 
 const AllProducts = () => {
+    const [openUploadProduct, setOpenUploadProduct] = useState(false)
     return (
-        <div>All Products</div>
+        <div className='p-2'>
+            <div className='bg-white py-2 px-4 flex justify-between items-center'>
+                <h2 className='font-bold  text-lg'>All Products</h2>
+                <button className='border-2 border-black hover:bg-black hover:text-white py-1 px-3 rounded-full' onClick={()=>setOpenUploadProduct(true)}>Update Product</button>
+            </div>
+
+            {/* Upload product component */}
+            {
+                openUploadProduct && (
+                    <UploadProduct onClose={()=>setOpenUploadProduct(false)} />
+                )
+            }
+            
+        </div>
     )
 }
 
